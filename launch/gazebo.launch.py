378c760d924d2b5dca225a91d7b39aece4f9eadc
Beren <akpinarbe22@gmail.com>
2026-01-04 06:04:48 +0000

    Changed mass and inertia values of baselink and fixed path issues for meshes

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -1,16 +1,23 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, TimerAction
+from launch.actions import ExecuteProcess, TimerAction, AppendEnvironmentVariable
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
     pkg_path = get_package_share_directory('urdf_description')
-    xacro_file = os.path.join(pkg_path, 'urdf', 'URDF.xacro')
 
+    set_resource_path = AppendEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=os.path.join(pkg_path, '..')
+    )
+
+    xacro_file = os.path.join(pkg_path, 'urdf', 'URDF.xacro')
     robot_description = os.popen(f'xacro {xacro_file}').read()
 
     return LaunchDescription([
+
+        set_resource_path,
 
         # Start Gazebo
         ExecuteProcess(cmd=['ign', 'gazebo', '-r', 'empty.sdf'], output='screen'),
